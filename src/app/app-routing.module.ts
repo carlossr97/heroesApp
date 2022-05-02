9544be3d29f811aa3931c828fbb07a3e0e2db0e7
Carlos Agustin Rodriguez <carodriguez@hospitalaleman.com>
2022-05-02 17:20:55 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from "../app/shared/error-page/error-page.component";
+const routes: Routes = [
 
-const routes: Routes = [];
+  {
+    path:'auth',
+    loadChildren:()=> import('../app/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:'heroes',
+    loadChildren:()=> import('../app/heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path:'404',
+    component: ErrorPageComponent
+  },
+  {
+    path:'**',
+    redirectTo:'404'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
