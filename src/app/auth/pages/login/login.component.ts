db9177d363ed887a61617821960b44517ba73b62
carlossr97 <carlosagustin.rodriguez@comunidad.ub.edu.ar>
2022-07-05 00:52:25 -0300

    Se agrego un servicio de auth para login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,
+              private auth:AuthService                      ) { }
 
   ngOnInit(): void {
   }
 
+  login(){
+
+
+    this.auth.login().subscribe(
+      ({id}) =>{console.log("respuesta: ",id)
+      if(id > 0){
+         this.router.navigate(['./heroes']);
+      }
+     
+    },
+    (error)=>{
+      alert("ERROR EN LA PETICION")
+    }
+    )
+    // this.router.navigate(['./heroes']);
+
+  }
+
+  
 }
+
